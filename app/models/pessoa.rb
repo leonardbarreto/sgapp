9a876cfe29086f343f740c8aa9dfd94913965512
Leonard <leonardbarreto@gmail.com>
2014-01-20 13:43:48 -0200

    acerto telefonecel em pessoas

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -11,7 +11,7 @@ class Pessoa < ActiveRecord::Base
   validates_presence_of :cor_id, :estado_civil_id, :municipio_id,:nome
   validates_length_of :identidade, :maximum=>10, :allow_nil=>true
   validates_length_of :telefone, :maximum=>10, :allow_nil=>true
-  validates_length_of :telefonecel, :maximum=>10, :allow_nil=>true
+  validates_length_of :telefonecel, :maximum=>15, :allow_nil=>true
   SEXO=%w[M F]
 	COR=%w[NEGRO BRANCA PARDA]
 	SIMNAO=%W[S N -]
